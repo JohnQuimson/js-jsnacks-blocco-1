573fbf4a42f8c2b3a5df48894aa800e39b8ad496
JohnQuimson <john.quimson777@gmail.com>
2023-11-10 11:23:32 +0100

    feat: snack1

---
+++ snack2/main.js
@@ -0,0 +1,18 @@
+'use strict';
+
+//Input
+const input1 = prompt('Digita parola 1');
+const input2 = prompt('Digita parola 2');
+
+//Estraggo le lunghezze
+const lungh1 = input1.length;
+const lungh2 = input2.length;
+
+//Condizioni
+if (lungh1 === lungh2) {
+  console.log('Le parole sono lunghe uguali');
+} else if (lungh1 > lungh2) {
+  console.log(input1);
+} else {
+  console.log(input2);
+}
