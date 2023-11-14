13c58fb16591dc8546ed26cec00a4db09254c9d8
JohnQuimson <john.quimson777@gmail.com>
2023-11-14 12:39:20 +0100

    feat: snack5

---
+++ snack5/main.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const inputUtente = Number(prompt('Quanti N array vuoi generare ?'));
+
+//verifica che l'input sia un numero
+if (!isNaN) {
+  //Genero inputUtente array
+  for (let i = 0; i < inputUtente; i++) {
+    const myArray = [];
+    //Pusho nell'array 10 numeri casuali
+    for (let j = 0; j < 10; j++) {
+      myArray.push(Math.floor(Math.random() * 100) + 1);
+    }
+    console.log(`Array ${i + 1}: [${myArray}]`);
+  }
+} else {
+  alert('Non hai inserito un numero');
+}
